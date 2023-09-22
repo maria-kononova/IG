5bcd017ecb74b65f287523575ed2aa9d48fa44ec
Lidia <lidia.samorukova@yandex.ru>
2023-09-22 12:34:39 +0300

    Test commit

--- src/main/java/com/example/ig/IgApplication.java
+++ src/main/java/com/example/ig/IgApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IgApplication {
-
+    String name = "IG";
     public static void main(String[] args) {
         SpringApplication.run(IgApplication.class, args);
     }

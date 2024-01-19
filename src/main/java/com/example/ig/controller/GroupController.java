bc95c8ecff0dd2833134c4657d4c36f579a91379
Maria <515nonia515@gmail.com>
2024-01-19 10:28:17 +0300

    groups update with cookie user data

--- src/main/java/com/example/ig/controller/GroupController.java
+++ src/main/java/com/example/ig/controller/GroupController.java
@@ -1,20 +1,47 @@
 package com.example.ig.controller;
 
 import com.example.ig.repository.GroupRepository;
+import com.example.ig.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import static com.example.ig.IgApplication.user;
+
 @Controller
 public class GroupController {
     private final GroupRepository groupRepository;
+    private final UserRepository userRepository;
+
     @Autowired
-    public GroupController(GroupRepository groupRepository) {
+    public GroupController(GroupRepository groupRepository, UserRepository userRepository) {
         this.groupRepository = groupRepository;
+        this.userRepository = userRepository;
+    }
+    public Long getUserFromCookie(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        String cookieName = "userId";
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (cookieName.equals(c.getName())) {
+                    if (!c.getValue().equals("")) {
+                        return Long.valueOf(c.getValue());
+                    }
+                }
+            }
+        }
+        return null;
     }
     @GetMapping("/group")
-    public String groupPage(Model model){
+    public String groupPage(Model model, HttpServletRequest request, HttpServletResponse response){
+        Long userId = getUserFromCookie(request, response);
+        if (userId != null) user = userRepository.getById(userId);
+        model.addAttribute("groups", groupRepository.findAll());
+        model.addAttribute("userLogin", user);
         model.addAttribute("groups", groupRepository.findAll());
         return "groups";
     }
--- src/main/java/com/example/ig/entity/User.java
+++ src/main/java/com/example/ig/entity/User.java
@@ -72,4 +72,5 @@ public class User {
         }
         return sb.toString();
     }
+
 }
